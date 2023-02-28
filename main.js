30e4b2ec59ddbdc150233780c04f19490abcbe76
alby0927 <istar102@gmail.com>
2023-02-28 15:28:09 +0900

    HTML: porfolio

---
+++ main.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbarMenu.classList.remove('open');
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+// Handle scrolling
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  scrollIntoView(link);
+});
+
+// Navbar toggle button
+const navbarToggleBtn = document.querySelector(
+  '.navbar__toggle_button'
+);
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+// contactMe button
+const contactMe = document.querySelector('.home__contact');
+contactMe.addEventListener('click', (e) => {
+  scrollIntoView('#contact');
+});
+
+// Home slowly fade to transparent when scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Show 'arrow up'
+const arrowUp = document.querySelector('.arrow-Up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+// Handle click arrow-Up button
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
